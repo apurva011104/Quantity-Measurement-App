8f957b6d11e4cdc0abcfe4176c5a242be8030419
apurva011104 <akaul7817@gmail.com>
2026-04-02 11:46:16 +0530

    [Apurva Gupta] Connected Database to supabase

--- src/main/java/com/apps/quantity_measurement_app/Application.java
+++ src/main/java/com/apps/quantity_measurement_app/Application.java
@@ -15,6 +15,11 @@ public class Application {
 		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
         System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
         System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		/*
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		*/
 		
 		SpringApplication.run(Application.class, args);
 		System.out.println("Application started");
